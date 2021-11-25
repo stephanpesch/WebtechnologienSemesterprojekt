2310680f7876cb3503665c39ed17e3ffa1833ecc
stephanpesch <stephan.pesch1@gmail.com>
2021-11-25 09:55:46 +0100

    Changed some stuff

--- gast_formular.php
+++ gast_formular.php
@@ -20,12 +20,12 @@
     ?>
     <main class="container">
         <h1>Gästeregistrierung</h1>
-        <form action="register.php" method="POST">
+        <form action="gast_formular.php" method="POST">
             <div class="row g-3">
                 <div class="col-12">
                     <label for="anrede" class="form-label">Anrede</label>
-                    <select id="anrede" name="anrede" class="form-select" required>
-                        <option selected>Auswählen...</option>
+                    <select id="anrede" name="anrede" class="form-select <?=$_POST["anrede"] == "invalid" ? : "is-invalid"?>" required>
+                        <option value="invalid" selected>Auswählen...</option>
                         <option value="herr">Herr</option>
                         <option value="frau">Frau</option>
                         <option value="keine">keine Anrede</option>
@@ -34,12 +34,12 @@
 
                 <div class="col-sm-6">
                     <label for="firstName" class="form-label">Vorname</label>
-                    <input type="text" id="firstName" name="firstName" class="form-control" required>
+                    <input type="text" id="firstName" name="firstName" class="form-control is-invalid">
                 </div>
 
                 <div class="col-sm-6">
                     <label for="lastName" class="form-label">Nachname</label>
-                    <input type="text" id="lastName" name="lastName" class="form-control" required>
+                    <input type="text" id="lastName" name="lastName" class="form-control">
                 </div>
 
                 <div class="col-12">
@@ -49,27 +49,51 @@
 
                 <div class="col-md-6">
                     <label for="street" class="form-label">Straße</label>
-                    <input type="text" id="street" name="street" class="form-control" required>
+                    <input type="text" id="street" name="street" class="form-control">
                 </div>
 
                 <div class="col-md-6">
                     <label for="houseNumber" class="form-label">Hausnummer</label>
-                    <input type="text" id="houseNumber" name="houseNumber" class="form-control" required>
+                    <input type="text" id="houseNumber" name="houseNumber" class="form-control">
                 </div>
 
                 <div class="col-md-2">
                     <label for="zipCode">Postleitzahl</label>
-                    <input type="text" id="zipCode" name="zipCode" class="form-control" required>
+                    <input type="text" id="zipCode" name="zipCode" class="form-control">
                 </div>
 
                 <div class="col-md-6">
                     <label for="location">Ort</label>
-                    <input type="text" id="location" name="location" class="form-control" required>
+                    <input type="text" id="location" name="location" class="form-control">
                 </div>
-                <button type="submit" class="btn btn-primary">Absenden</button>
+                <button type="submit" value="login" class="btn btn-primary">Absenden</button>
             </div>
         </form>
     </main>
+
+    <?php
+        var_dump($_POST);
+        if (isset($_POST["login"])) {
+            var_dump($_POST);
+            echo "<br>";
+            echo $_POST["anrede"];
+            echo "<br>";
+            echo $_POST["firstName"];
+            echo "<br>";
+            echo $_POST["lastName"];
+            echo "<br>";
+            echo $_POST["email"];
+            echo "<br>";
+            echo $_POST["street"];
+            echo "<br>";
+            echo $_POST["houseNumber"];
+            echo "<br>";
+            echo $_POST["zipCode"];
+            echo "<br>";
+            echo $_POST["location"];
+        }
+    ?>
+
     <!--Bottom Navbar-->
     <?php
     include("./components/navbar_bottom.php");
